27177207f8fbac83ca8a83af74e156555060b807
vesilth <vesilth@gmail.com>
2022-03-20 21:32:54 +0000

    add login logic

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
+from flask_login import LoginManager
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
@@ -10,7 +13,11 @@ def home():
 def login():
 	from forms import LoginForm
 	form = LoginForm(request.form)
-	
+	if request.method == 'POST' and form.validate():
+		user = User(form.username.data,
+					form.password.data)
+		# ask DB if credentials are ok
+		return redirect(url_for('/'))	
 	return render_template('login.html', form=form)
 
 @app.route('/xd')
