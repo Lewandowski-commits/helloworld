4fb8ba5c745dbcf416ebc1bb476c5bec953690e9
vesilth <vesilth@gmail.com>
2022-03-20 00:34:17 +0000

    correct service name

--- reload.py
+++ reload.py
@@ -2,4 +2,4 @@ import os
 
 os.system('systemctl daemon-reload')
 os.system('systemctl restart nginx')
-os.system('systemctl restart flaga.service')
+os.system('systemctl restart helloworld.service')
