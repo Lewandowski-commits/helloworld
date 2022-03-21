ea34d50839f7ca08b28f11c837af767e3d5f9939
vesilth <vesilth@gmail.com>
2022-03-21 22:15:50 +0000

    escape password

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 from os import getenv
+from urllib.parse import quote
 
 def get_database():
-    CONNECTION_STRING = f'mongodb+srv://login:{getenv("MONGODB_PASSWORD")}@cluster0.uivfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    CONNECTION_STRING = f'mongodb+srv://login:{quote(getenv("MONGODB_PASSWORD"))}@cluster0.uivfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     client = MongoClient(CONNECTION_STRING)
 
     return client
 
 if __name__ == '__main__':
-    get_database().test
+    print(get_database().test)
