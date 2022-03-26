a268b28eb00be26aa19db31db627a9f64b3b8857
vesilth <vesilth@gmail.com>
2022-03-26 15:23:08 +0000

    explicitly stringify username

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ class Db:
     def __init__(self, USR=getenv("MONGODB_USER"), PWD=getenv("MONGODB_PASSWORD")):
         self.USR = USR
         self.PWD = PWD
-        CONNECTION_STRING = f'mongodb+srv://{self.USR}:{quote(str(self.PWD))}@cluster0.uivfa.mongodb.net/blog?retryWrites=true&w=majority'
+        CONNECTION_STRING = f'mongodb+srv://{quote(str(self.USR))}:{quote(str(self.PWD))}@cluster0.uivfa.mongodb.net/blog?retryWrites=true&w=majority'
         client = pymongo.MongoClient(CONNECTION_STRING)
         self.client = client
     
