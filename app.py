a67188df327c501a191f7c42303265b2fe3f09b5
Lewandowski-commits <michalewandowski0@gmail.com>
2022-09-02 16:16:38 +0200

    add host and port

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ app.layout = html.Div([
 )
 
 if __name__ == '__main__':
-	app.run_server(debug=getenv('DASH_DEBUG_MODE', True))
+	app.run_server(debug=getenv('DASH_DEBUG_MODE', True), host='0.0.0.0', port='8050')
