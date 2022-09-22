27feefe28203f21d77dd63aa5e0010cbc8814277
Lewandowski-commits <michalewandowski0@gmail.com>
2022-09-22 19:37:24 +0200

    import boto3 and add call to dynamodb

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
 """Routes for parent Flask app."""
 from flask import render_template
 from flask import current_app as app
+import boto3
 
+client = boto3.client('dynamodb')
+
+dynamodb = boto3.resource('dynamodb')
+
+positions = dynamodb.Table('job_history').scan()['Items']
 
 @app.route('/')
 def home():
     """Landing page."""
     return render_template(
-        'index.html'
+        'index.html',
+        positions=positions
     )
