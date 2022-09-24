fd89ce13811971d5f69e1f8b51fba457570a04bb
Lewandowski-commits <michalewandowski0@gmail.com>
2022-09-24 18:22:53 +0200

    remove positions and timeline

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,13 @@
 """Routes for parent Flask app."""
 from flask import render_template
 from flask import current_app as app
-import boto3
-
-client = boto3.client('dynamodb')
-
-dynamodb = boto3.resource('dynamodb')
 
 @app.route('/')
 def home():
     """Landing page."""
-    positions = dynamodb.Table('job_history').scan()['Items']
-
-    positions = reversed(positions)
+    companies = ['gsk', 'haleon', 'lingaro']
 
     return render_template(
         'index.html',
-        positions=positions
+        companies=companies
     )
